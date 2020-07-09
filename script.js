b12149779608c9fe40af287a6d1539569a8e349c
pjmantoss <joelptoss@gmail.com>
2020-07-09 16:04:11 +0100

    update script

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let songs = new Audio;
-let muted = false;
-let vol = 1;
+let song = new Audio,
+    muted = false,
+    vol = 1;
+
+song.type = 'audio/mpeg';
+song.src = 'https://www.bensound.com/bensound-music/bensound-summer.mp3';
+
+let skip = time => {
+    if(time == 'back'){
+        song.currentTime = (song.currentTime - 5);
+    }else if(time == 'fwd'){
+        song.currentTime = (song.currentTime + 5);
+    }
+}
